f4d7b9a34e6c85c468f68fa7d0857bf51baa40cf
familyidfinder <anjuyadavkp81@gmail.com>
2025-07-23 15:03:19 +0530

    Delete restore.js

--- restore.js
+++
@@ -1,9 +0,0 @@
-const MAX_SESSION_TIME = 30 * 60 * 1000; // 30 minutes
-  const isLoggedIn = sessionStorage.getItem("isLoggedIn");
-  const loginTime = sessionStorage.getItem("loginTime");
-
-  if (!isLoggedIn || isLoggedIn !== "true" || !loginTime || (Date.now() - loginTime > MAX_SESSION_TIME)) {
-    sessionStorage.clear();
-  
-    window.location.href = ""index.html";
-  }
